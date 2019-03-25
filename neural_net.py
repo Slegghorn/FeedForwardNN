29b2041797a2f6f5a5e94ab2095056c2dba71a5f
Slegghorn <yarneiluj@yahoo.com>
2019-03-25 19:20:00 +0100

    v0.1 : w1 sgd done

--- neural_net.py
+++ neural_net.py
@@ -1,12 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.array([[1], [2]]).reshape(1,2)
+x = np.array([[1, 0]]).reshape(1,2)
+y = np.array([[1]]).reshape(1, 1)
+
+lr = 0.03
+epochs = 100
+
 
 def Relu(x):
     return x * (x > 0)
 def dRelu(x):
     return 1 * (x > 0)
+
+def sig(x):
+    return 1 / (1 + np.exp(-x))
+def dsig(x):
+    return np.exp(-x) / ((1 + np.exp(-x))**2)
+
+def MSE(y_p, y):
+    return (y - y_p)**2
 
 class NeuralNetwork():
 
@@ -14,16 +27,50 @@ class NeuralNetwork():
         self.w1 = np.random.normal(size = (2, 4))
         self.b1 = np.random.normal(size = (4))
 
-        self.w2 = np.random.normal(size = (4))
+        self.w2 = np.random.normal(size = (4, 1))
         self.b2 = np.random.normal(size = (1))
 
     def feedforward(self, x):
-        h = Relu(np.matmul(x, self.w1) + self.b1)
-        return Relu(np.matmul(h, self.w2) + self.b2)
+        h = Relu(np.dot(x, self.w1) + self.b1)
+        print(h)
+        return sig(np.dot(h, self.w2) + self.b2)
 
     def train(self, x_train, y_train):
-        -1
+        for i in range(epochs):
+            for x, y in zip(x_train, y_train):
+                #feedforward
+                l_1 = np.dot(x, self.w1) + self.b1
+                h = Relu(l_1)
+
+                l_2 = np.dot(h, self.w2) + self.b2
+                o = sig(l_2)
+
+                delta1 = dRelu(l_1[0]) *self.w2[0] * dsig(l_2) * -2*(y - o)
+                delta2 = dRelu(l_1[1]) *self.w2[1] * dsig(l_2) * -2*(y - o)
+                delta3 = dRelu(l_1[2]) *self.w2[2] * dsig(l_2) * -2*(y - o)
+                delta4 = dRelu(l_1[3]) *self.w2[3] * dsig(l_2) * -2*(y - o)
+
+                self.w1[0][0] -= x[0] * lr * delta1
+                self.w1[1][0] -= x[1] * lr * delta1
+
+                self.w1[0][1] -= x[0] * lr * delta2
+                self.w1[1][1] -= x[1] * lr * delta2
+
+                self.w1[0][2] -= x[0] * lr * delta3
+                self.w1[1][2] -= x[1] * lr * delta3
+
+                self.w1[0][3] -= x[0] * lr * delta4
+                self.w1[1][3] -= x[1] * lr * delta4
+
+                self.b1[0] -= lr * delta1
+                self.b1[1] -= lr * delta2
+                self.b1[2] -= lr * delta3
+                self.b1[3] -= lr * delta4
+                print((y-o)**2)
+
+
 
 
 
 nn = NeuralNetwork()
+nn.train(x, y)
